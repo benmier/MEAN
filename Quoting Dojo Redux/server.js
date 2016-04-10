f0345aac658dc17927b016981e8db06f8489b86b
benmier <bencmier@gmail.com>
2016-04-10 14:02:39 -0700

    Update

--- Quoting Dojo Redux/server.js
+++ Quoting Dojo Redux/server.js
@@ -9,43 +9,66 @@ app.use(express.static(__dirname + "./static"));
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
 
-mongoose.connect('mongodb://localhost/quoting_dojo');
+mongoose.connect('mongodb://localhost/kitten_dashboard');
 
 app.listen(8000, function(){})
 var Schema = new mongoose.Schema({
     name: String,
-    quote: String,
+    breed: String,
+    color: String,
+    personality: String,
+    age: Number,
+    food: String,
+    toy: String,
+    id: Number
 },{timestamps: true})
-mongoose.model('Quote', Schema);
-var Quote = mongoose.model('Quote')
+mongoose.model('Kitten', Schema);
+var Kitten = mongoose.model('Kitten')
 
 app.get('/', function(req, res) {
-    res.render('index');
-});
-
-app.get('/quotes', function(req, res) {
-    Quote.find({},function(err,quotes){
-        console.log(quotes)
+    Kitten.find({},function(err,kittens){
+        console.log(kittens)
         if(err)
             console.log("Error matching DB request")
         else
-            res.render('quotes', {all_quotes:quotes});
+            res.render('index', {kittens:kittens});
     }).sort({_id:-1});
 });
 
-app.post('/quotes', function(req, res) {
-    var new_quote = new Quote({
+app.get('/kittens/new', function(req, res) {
+    res.render('new');
+});
+
+app.get('/kittens/:id', function(req, res) {
+    Kitten.findOne({id:req.params.id},function(err,kitten){
+        console.log(kitten)
+        if(err)
+            console.log("Error matching DB request")
+        else
+            res.render('show', {kitten:kitten});
+    });
+});
+
+app.post('/kittens', function(req, res) {
+    var new_kitten = new Kitten({
     	name: req.body.name,
-    	quote: req.body.quote
+        breed: req.body.breed,
+        color: req.body.color,
+        personality: req.body.personality,
+        age: req.body.age,
+        food: req.body.food,
+        toy: req.body.toy,
+        id: id
     });
-    new_quote.save(function(err){
+    id++;
+    new_kitten.save(function(err){
     	if(err)
-    		errors = err;
+    		console.log("Error inserting into DB")
     });
-    Quote.find({},function(err,quotes){
+    Quote.findOne({},function(err,quotes){
         if(err)
             console.log("Error matching DB request")
         else
-            res.render('quotes', {all_quotes:quotes});
-    }).sort({_id:-1});
+            res.render('show', {kitten:kitten});
+    }).sort({id:-1});
 })
