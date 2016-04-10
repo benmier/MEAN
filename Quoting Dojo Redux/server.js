ace78191941db9782877165b35fe52640b8ca806
benmier <bencmier@gmail.com>
2016-04-09 17:28:03 -0700

    Update

--- Quoting Dojo Redux/server.js
+++ Quoting Dojo Redux/server.js
@@ -1,32 +1,28 @@
-// Require the Express Module
 var express = require("express");
-// Create an Express App
 var app = express();
-// Require body-parser (to receive post data from clients)
 var bodyParser = require("body-parser");
-
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/basic_mongoose');
-// Integrate body-parser with our App
-app.use(bodyParser.urlencoded());
-// Require path
 var path = require("path");
-// Setting our Static Folder Directory
+
+app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + "./static"));
-// Setting our Views Folder Directory
 app.set('views', path.join(__dirname, './views'));
-// Setting our View Engine set to EJS
 app.set('view engine', 'ejs');
-// Routes
-// Root Request
-app.get('/', function(req, res) {
-    User.find({},function(err,users){
-    	if(err)
-    		console.log("Error matching DB request")
-    	else
-    		res.render('index', {info: users});
-    });
+
+mongoose.connect('mongodb://localhost/quoting_dojo');
+
+app.listen(8000, function(){})
+var UserSchema = new mongoose.Schema({
+    name: String,
+    age: Number
 })
+mongoose.model('User', UserSchema);
+var User = mongoose.model('User')
+
+app.get('/', function(req, res) {
+    res.render('index');
+});
+
 // Add User Request 
 app.post('/users', function(req, res) {
     console.log("POST DATA", req.body);
@@ -41,13 +37,3 @@ app.post('/users', function(req, res) {
     		res.redirect('/');
     });
 })
-app.listen(8000, function() {
-    console.log("listening on port 8000");
-})
-
-var UserSchema = new mongoose.Schema({
-	name: String,
-	age: Number
-})
-mongoose.model('User', UserSchema);
-var User = mongoose.model('User')
