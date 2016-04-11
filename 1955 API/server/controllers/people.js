785531a87b32ec4bafa0dcebda51883a9d60d90f
benmier <bencmier@gmail.com>
2016-04-11 16:26:08 -0700

    Updated MEAN 1955 API project

--- 1955 API/server/controllers/people.js
+++ 1955 API/server/controllers/people.js
@@ -4,11 +4,21 @@ var People = mongoose.model('People');
 module.exports = {
 
 	show: function(req,res){
-
+		People.find({},function(err,people){
+			if(err)
+				req.json(err);
+			else
+				req.json(people);
+		});
 	},
 
 	show_one: function(req,res){
-
+		People.findOne({name:req.params.name},function(err,person){
+			if(err)
+				req.json(err);
+			else
+				req.json(person);
+		});
 	},
 
 	create: function(req,res){
