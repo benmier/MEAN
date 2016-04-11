563b9f247cf8f70306bbaca634eee150b5677e24
benmier <bencmier@gmail.com>
2016-04-11 12:26:34 -0700

    Updated MEAN quoting dojo redux with modularization project

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 for i in {1..144}
 do
 	git add .
-	git commit -m 'Updated MEAN message board project'
+	git commit -m 'Updated MEAN quoting dojo redux with modularization project'
 	git push
 	sleep 300
 done
