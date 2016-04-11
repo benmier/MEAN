ba7338571fce3fe525a88315c3f53dd670c7ed82
benmier <bencmier@gmail.com>
2016-04-11 09:23:38 -0700

    Updated MEAN message board project

--- Group Chat/server.js
+++ Group Chat/server.js
@@ -11,23 +11,19 @@ app.set('view engine', 'ejs');
 
 var server = app.listen(8000, function(){})
 var io = require('socket.io').listen(server);
-var id = 0;
 var messages = {};
-var users = {};
+
 io.sockets.on('connection', function (socket) {
-	socket.on("got_new_user", function(data){
-		users[socket.id] = {name:data.name};
-		socket.emit('existing_messages', messages);
-		io.emit("display_new_user", {name:data.name})
-	});
-	socket.on("new_message", function(data){
-		messages[id] = {name:data.name, message:data.message};
-		io.emit("update_messages", messages[id]);
-		id++;
-	})
-	socket.on("disconnect", function(){
-		io.emit("user_disconnect", users[socket.id])
-	})
+	socket.emit('existing_messages', messages);
+});
+
+socket.on("new_message", function(data){
+	messages[id] = {name:data.name, message:data.message};
+	io.emit("update_messages", messages[id]);
+	id++;
+})
+socket.on("disconnect", function(){
+	io.emit("user_disconnect", users[socket.id])
 });
 
 app.get('/', function(req, res) {
