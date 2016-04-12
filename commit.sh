8f15cff9a199d79cd4a9980505895edd993c09ef
benmier <bencmier@gmail.com>
2016-04-12 12:57:12 -0700

    Updated MEAN products with a factory project

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 for i in {1..144}
 do
 	git add .
-	git commit -m 'Updated MEAN users with controllers project'
+	git commit -m 'Updated MEAN products with a factory project'
 	git push
 	sleep 300
 done
