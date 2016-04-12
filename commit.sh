6a7fc4e8f1e68802f672bddd195a06a0bfd5eb6f
benmier <bencmier@gmail.com>
2016-04-12 16:44:56 -0700

    Updated MEAN customizing users with partials project

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 for i in {1..144}
 do
 	git add .
-	git commit -m 'Updated MEAN products and quantities project'
+	git commit -m 'Updated MEAN customizing users with partials project'
 	git push
 	sleep 300
 done
