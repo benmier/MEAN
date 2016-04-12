d0acfc602697cc7fba2666dc969517af85104746
benmier <bencmier@gmail.com>
2016-04-12 15:11:34 -0700

    Updated MEAN products and quantities project

--- commit.sh
+++ commit.sh
@@ -2,7 +2,7 @@
 for i in {1..144}
 do
 	git add .
-	git commit -m 'Updated MEAN products with a factory project'
+	git commit -m 'Updated MEAN products and quantities project'
 	git push
 	sleep 300
 done
