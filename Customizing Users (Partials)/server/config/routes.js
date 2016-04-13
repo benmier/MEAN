cf60d440069b9ed9124f7fdacb0bf216818c17a9
benmier <bencmier@gmail.com>
2016-04-12 17:30:37 -0700

    Updated MEAN customizing users with partials project

--- Customizing Users (Partials)/server/config/routes.js
+++ Customizing Users (Partials)/server/config/routes.js
@@ -3,4 +3,9 @@ module.exports = function(app){
 	app.get('/', function(req, res) {
 	    res.render('index')
 	});
+
+	app.get('#/customize_users', function(req, res) {
+		console.log("hit")
+	    res.render('customizeUsers.html')
+	});
 };
