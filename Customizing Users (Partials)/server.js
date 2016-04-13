b2ebe29981b0f47b59be7085a0864b7146beec44
benmier <bencmier@gmail.com>
2016-04-12 18:41:35 -0700

    Updated MEAN customizing users with partials project

--- Customizing Users (Partials)/server.js
+++ Customizing Users (Partials)/server.js
@@ -1,9 +1,6 @@
 var express = require("express");
 var app = express();
-var bodyParser = require("body-parser");
-var path = require("path");
 
-app.use(bodyParser.urlencoded());
 app.use(express.static(__dirname + "/client"));
 
 app.listen(8000, function(){console.log("listening on port 8000")});
