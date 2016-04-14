5fc069324f94244ad9aa084396ae0845046133bb
benmier <bencmier@gmail.com>
2016-04-14 08:27:34 -0700

    Updated MEAN friends demo project

--- Mean Demo/server.js
+++ Mean Demo/server.js
@@ -1,9 +1,14 @@
-var express = require("express");
+// require express so that we can build an express app
+var express = require('express');
+// require path so that we can use path stuff like path.join
+var path = require('path');
+// instantiate the app
 var app = express();
 var bodyParser = require('body-parser'); 
-var path = require('path');
-
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, './client')));
 
-app.listen(8000, function(){console.log("listening on port 8000")});
+// set up a static file server that points to the "client" directory
+app.use(express.static(path.join(__dirname, './client')));
+app.listen(8000, function() {
+  console.log('cool stuff on: 8000');
+});
