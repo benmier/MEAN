3e62fce0718f0f50783a7ba3166e978cf4811b2b
benmier <bencmier@gmail.com>
2016-04-14 13:42:35 -0700

    Updated MEAN 1955 API redux project

--- Mean Demo/server.js
+++ Mean Demo/server.js
@@ -6,5 +6,7 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, './client')));
 
+require('./server/config/mongoose.js');
 require('./server/config/routes.js')(app);
-app.listen(8000, function(){console.log('Listening on port: 8000')});
+
+app.listen(8000, function(){console.log('Listening on: 8000');});
