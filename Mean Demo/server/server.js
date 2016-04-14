cec575e6286e2ef092b49ccd082bb6e87b113e3c
benmier <bencmier@gmail.com>
2016-04-14 09:00:17 -0700

    Updated MEAN friends demo project

---
+++ Mean Demo/server/server.js
@@ -0,0 +1,17 @@
+// require express so that we can build an express app
+var express = require('express');
+// require path so that we can use path stuff like path.join
+var path = require('path');
+// instantiate the app
+var app = express();
+var bodyParser = require('body-parser'); 
+app.use(bodyParser.json());
+
+// this line requires and runs the code from our routes.js file and passes it app so that we can attach our routing rules to our express application!
+require('./server/config/routes.js')(app);
+
+// set up a static file server that points to the "client" directory
+app.use(express.static(path.join(__dirname, './client')));
+app.listen(8000, function() {
+  console.log('cool stuff on: 8000');
+});
