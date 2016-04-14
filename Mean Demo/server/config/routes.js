87d2b90fc7367308c7f42ebe6e0761f6028ce932
benmier <bencmier@gmail.com>
2016-04-14 11:07:50 -0700

    Updated MEAN friends demo project

--- Mean Demo/server/config/routes.js
+++ Mean Demo/server/config/routes.js
@@ -2,7 +2,7 @@ var friends = require('./../controllers/friends.js');
 
   module.exports = function(app) {
   // verb: get, plural of target as the URI is the RESTful index method (it returns all friends)
-    app.patch('/friends', function(req, res) {
+    app.get('/friends', function(req, res) {
     	friends.index(req,res);
     });
 
