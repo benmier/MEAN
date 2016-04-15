26a6e8ff751282a31ecd638bc7821cc7054f7d0e
benmier <bencmier@gmail.com>
2016-04-15 15:19:07 -0700

    Updated MEAN mini mean store project

--- Mini Mean Store/server/controllers/products.js
+++ Mini Mean Store/server/controllers/products.js
@@ -3,31 +3,31 @@ var Products = mongoose.model('Products');
 
 module.exports = {
 
-    // show: function(req,res){
-    //     Orders.find({},function(err,data){
-    //         if(err)
-    //             console.log(err);
-    //         else
-    //             res.json(data);
-    //     });
-    // },
+    show: function(req,res){
+        Products.find({},function(err,data){
+            if(err)
+                console.log(err);
+            else
+                res.json(data);
+        });
+    },
 
-    // create: function(req,res){
-    //     var order = new Orders(req.body) 
-    //     order.save(function(err){
-    //         if(err)
-    //             console.log(err);
-    //         else
-    //             res.redirect('/orders')
-    //     });
-    // },
+    create: function(req,res){
+        var product = new Products(req.body) 
+        product.save(function(err){
+            if(err)
+                console.log(err);
+            else
+                res.redirect('/products')
+        });
+    },
 
-    // delete: function(req,res){
-    //     Orders.remove({name:req.body.name},function(err){
-    //         if(err)
-    //             console.log(err);
-    //         else
-    //             res.json({status:true});
-    //     })
-    // },
+    delete: function(req,res){
+        Products.remove({_id:req.body._id},function(err){
+            if(err)
+                console.log(err);
+            else
+                res.json({status:true});
+        })
+    },
 }
