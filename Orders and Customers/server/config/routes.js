657f59ebea19a23455aa6fcd1986d1f442dc62cd
benmier <bencmier@gmail.com>
2016-04-15 11:48:07 -0700

    Updated MEAN orders and customers project

--- Orders and Customers/server/config/routes.js
+++ Orders and Customers/server/config/routes.js
@@ -10,5 +10,15 @@ module.exports = function(app){
   	app.get('/orders', function(req, res) {
       orders.show(req,res);
   	});
+
+  	app.post('/customers/new', function(req, res) {
+      customers.create(req,res);
+  	});
+
+  	app.post('/orders/new', function(req, res) {
+      orders.create(req,res);
+  	});
+
+
   
 };
