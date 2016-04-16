50fae34a44bccd76b508305878b0d57540be17bb
benmier <bencmier@gmail.com>
2016-04-16 15:59:23 -0700

    Updated MEAN user dashboard project

--- User Dashboard/client/script.js
+++ User Dashboard/client/script.js
@@ -23,4 +23,44 @@ myApp.config(function($routeProvider){
         })
 });
 
+myApp.factory('userFactory', function($http){
+    var factory = {}, users;
 
+    factory.show = function(callback){
+        $http.get('/users').success(function(data){
+            users = data;
+            callback(users);
+        });
+    };
+
+    return factory;
+});
+
+App.factory('threadFactory', function($http){
+    var factory = {}, threads;
+
+    factory.show = function(callback){
+        $http.get('/threads').success(function(data){
+            threads = data;
+            callback(threads);
+        });
+    };
+
+    return factory;
+});
+
+myApp.controller('loginController', function($scope,userFactory){
+
+});
+
+myApp.controller('dashboardController', function($scope,threadFactory){
+    
+});
+
+myApp.controller('threadController', function($scope,threadFactory){
+    
+});
+
+myApp.controller('userController', function($scope,userFactory, threadFactory){
+    
+});
