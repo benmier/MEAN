7fa5c548987ae50bacc74687bb9bbc7dd7a2bf96
benmier <bencmier@gmail.com>
2016-04-16 15:54:17 -0700

    Updated MEAN user dashboard project

--- User Dashboard/client/script.js
+++ User Dashboard/client/script.js
@@ -1,14 +1,26 @@
 var myApp = angular.module('myApp',['ngRoute']);
 
-        myApp.config(function($routeProvider){
-            $routeProvider
-                .when('/',{
-                    templateUrl: 'partials/login.html',
-                    controller: "loginController"
-                })
+myApp.config(function($routeProvider){
+    $routeProvider
+        .when('/',{
+            templateUrl: 'partials/login.html',
+            controller: "loginController"
+        })
+        .when('/dashboard',{
+            templateUrl: 'partials/dashboard.html',
+            controller: "dashboardController"
+        })
+        .when('/thread',{
+            templateUrl: 'partials/thread.html',
+            controller: "threadController"
+        })
+        .when('/user',{
+            templateUrl: 'partials/user.html',
+            controller: "userController"
+        })
+        .otherwise({
+            redirectTo: '/dashboard'
+        })
+});
 
-                .otherwise({
-                    redirectTo: '/'
-                })
-        });
 
