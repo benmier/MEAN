e9fdafdf902b9fab0155b2383275830636acd463
benmier <bencmier@gmail.com>
2016-04-16 14:08:18 -0700

    Updated MEAN mini mean store project

--- Mini Mean Store/client/script.js
+++ Mini Mean Store/client/script.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp',['ngRoute']);
+var myApp = angular.module('myApp',['ngRoute,timeRelative']);
 
         myApp.config(function($routeProvider){
             $routeProvider
