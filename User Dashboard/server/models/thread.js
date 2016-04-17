9635fa1f99d80fea772619b1b1a16a35294b7b1a
benmier <bencmier@gmail.com>
2016-04-17 16:54:44 -0700

    Updated MEAN user dashboard project

--- User Dashboard/server/models/thread.js
+++ User Dashboard/server/models/thread.js
@@ -1,2 +1,10 @@
 var mongoose = require('mongoose');
 
+var ThreadSchema = new mongoose.Schema({
+	name: String,
+	threads: Number,
+	answers: Number,
+	comments: Number
+},{timestamps:true});
+
+var Threads = mongoose.model('Threads', ThreadSchema);
--- User Dashboard/server/models/user.js
+++ User Dashboard/server/models/user.js
@@ -1,2 +1,10 @@
 var mongoose = require('mongoose');
 
+var UserSchema = new mongoose.Schema({
+	name: String,
+	threads: Number,
+	answers: Number,
+	comments: Number
+},{timestamps:true});
+
+var Users = mongoose.model('Users', UserSchema);
