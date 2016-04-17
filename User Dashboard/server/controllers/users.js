5b503fe62884105af1ad571940a9ce0367185ba2
benmier <bencmier@gmail.com>
2016-04-17 16:29:05 -0700

    Updated MEAN user dashboard project

---
+++ User Dashboard/server/controllers/users.js
@@ -0,0 +1,42 @@
+var mongoose = require('mongoose');
+var Users = mongoose.model('User');
+
+module.exports = {
+
+	show: function(req,res){
+        Users.find({},function(err,data){
+            if(err)
+                console.log(err);
+            else
+                res.json(data);
+        });
+    },
+
+    showOne: function(req,res){
+        Users.findOne({_id:req.params.id},function(err,data){
+            if(err)
+                console.log(err);
+            else
+                res.json(data);
+        });
+    },
+
+    create: function(req,res){
+        var user = new Users(req.body) 
+        user.save(function(err){
+            if(err)
+                console.log(err);
+            else{
+            	Users.findOne({},function(err,data){
+	            if(err)
+	                console.log(err);
+	            else
+	                res.json(data);
+	        	}).sort({_id:-1});
+            };
+        });
+    },
+
+
+
+}
