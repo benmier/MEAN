a7e39135b98a0d1625af04f99fd07b2e24ce8a8c
benmier <bencmier@gmail.com>
2016-04-19 10:05:55 -0700

    Updated MEAN black belt exam

--- Mean Exam/server/config/routes.js
+++ Mean Exam/server/config/routes.js
@@ -3,8 +3,36 @@ var polls = require('../controllers/polls.js');
 
 module.exports = function(app){
 
+	app.get('/users', function(req, res) {
+        users.show(req,res);
+    });
 
+    app.get('/polls', function(req, res) {
+        polls.show(req,res);
+    });
 
+    app.post('/users/create', function(req, res) {
+        users.create(req,res);
+    });
 
+    app.post('/polls/create', function(req, res) {
+        polls.create(req,res);
+    });
+
+    app.post('/polls/delete/:id', function(req, res) {
+        polls.delete(req,res);
+    });
+
+    app.get('/users/:id', function(req, res) {
+        users.showOne(req,res);
+    });
+
+    app.get('/polls/:id', function(req, res) {
+        polls.showOne(req,res);
+    });
+
+    app.post('/polls/vote/:id', function(req, res) {
+        polls.vote(req,res);
+    });
 
 }
---
+++ Mean Exam/server/controllers/polls.js
@@ -0,0 +1,47 @@
+var mongoose = require('mongoose');
+var Polls = mongoose.model('Polls');
+
+module.exports = {
+
+	show: function(req,res){
+        Polls.find({},function(err,data){
+            if(err)
+                console.log(err);
+            else
+                res.json(data);
+        });
+    },
+
+    showOne: function(req,res){
+        Polls.findOne({_id:req.params.id},function(err,data){
+            if(err)
+                console.log(err);
+            else
+                res.json(data);
+        });
+    },
+
+    create: function(req,res){
+        var poll = new Polls(req.body) 
+        poll.save(function(err){
+            if(err)
+                console.log(err);
+            else
+                res.redirect('/polls');
+        });
+    },
+
+    delete: function(req,res){
+        Polls.remove({_id:req.params.id},function(err,data){
+            if(err)
+                console.log(err);
+            else
+                res.redirect('/polls');
+        });
+    },
+
+
+
+
+
+}
