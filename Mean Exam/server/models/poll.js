8236884a8487ae142330fa474415b6bbb3b60eca
benmier <bencmier@gmail.com>
2016-04-19 12:03:54 -0700

    Updated MEAN black belt exam

--- Mean Exam/server/models/poll.js
+++ Mean Exam/server/models/poll.js
@@ -3,16 +3,16 @@ var mongoose = require('mongoose');
 var PollSchema = new mongoose.Schema({
 	name: {type:String},
 	question: {type:String,required:true,unique:true},
-	options: {
-			option1:{type:String},
-			option1votes: {type:Number,default:0},
-			option2:{type:String},
-			option2votes: {type:Number,default:0},
-			option3:{type:String},
-			option3votes: {type:Number,default:0},
-			option4:{type:String},
-			option4votes: {type:Number,default:0},
-		}
+	option1:{type:String},
+	option2:{type:String},
+	option3:{type:String},
+	option4:{type:String},
+	votes:[{
+		option1: {type:Number,default:0},
+		option2: {type:Number,default:0},
+		option3: {type:Number,default:0},
+		option4: {type:Number,default:0},
+		}]
 },{timestamps:true});
 
 var Polls = mongoose.model('Polls', PollSchema);
