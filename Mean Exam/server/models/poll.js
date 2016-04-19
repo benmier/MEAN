774f65e0c19546d9d0fadea6d75b007dfc2395bd
benmier <bencmier@gmail.com>
2016-04-19 12:09:01 -0700

    Updated MEAN black belt exam

--- Mean Exam/server/models/poll.js
+++ Mean Exam/server/models/poll.js
@@ -7,12 +7,12 @@ var PollSchema = new mongoose.Schema({
 	option2:{type:String},
 	option3:{type:String},
 	option4:{type:String},
-	votes:[{
-		option1: {type:Number,default:0},
-		option2: {type:Number,default:0},
-		option3: {type:Number,default:0},
-		option4: {type:Number,default:0},
-		}]
+	votes:[
+		{option: {type:Number,default:0}},
+		{option: {type:Number,default:0}},
+		{option: {type:Number,default:0}},
+		{option: {type:Number,default:0}},
+		]
 },{timestamps:true});
 
 var Polls = mongoose.model('Polls', PollSchema);
