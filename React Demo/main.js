9ddda8082f52ac79f258374f277a7f1981eca970
benmier <bencmier@gmail.com>
2016-04-20 11:12:39 -0700

    Updated react demo project

--- React Demo/main.js
+++ React Demo/main.js
@@ -1,18 +1,3 @@
-// create a new h1 element
-var h1Element = document.createElement("h1");
-// add content to the element
-var myContent = document.createTextNode("JavaScript Ninja");
-h1Element.appendChild(myContent); // add the text node to the newly created h1
-// Finally, grab an element from the DOM and add our newly created element
-document.getElementById('non-react-container').appendChild(h1Element);
-
-
-// Create an h1 element, passing null as the second parameter since we aren't adding properties, and the text
-// "React Ninja" as the new element's child.
-var reactComponent = React.createElement('h1', null, 'React Ninja');
-// Next use ReactDOM.render() to place the element to a location in the DOM.
-ReactDOM.render(reactComponent, document.getElementById('react-container'));
-
 var NinjaTitle = React.createClass({
  render: function(){
   return React.createElement('h1', null, this.props.title);
