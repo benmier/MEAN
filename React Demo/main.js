e50843ade0644fb1c956f5e1d4c621b33d8bba2d
benmier <bencmier@gmail.com>
2016-04-20 15:02:27 -0700

    Updated react demo project

--- React Demo/main.js
+++ React Demo/main.js
@@ -1,48 +1,63 @@
 var Header = React.createClass({
-	render: function(){
- 		return React.createElement('nav', {style: {border: "1px solid black"}}, 
- 			React.createElement('ul',null,
- 				React.createElement(ListItemWithLink,{name: "home"}),
- 				React.createElement(ListItemWithLink,{name: "Logout"}),
- 				React.createElement(ListItemWithLink,{name: "About Me"})
- 			)
- 		)
- 	}
-});
-
-var Sidebar = React.createClass({
-	render: function(){
- 		return React.createElement('div', {style: {border: "1px solid black", width:"20%", height:"100%", display:"inline-block"}}, 
- 			React.createElement('ul',null,
- 				React.createElement(ListItemWithLink,{name: "Link!"}),
- 				React.createElement(ListItemWithLink,{name: "Link!"}),
- 				React.createElement(ListItemWithLink,{name: "Link!"})
- 			)
- 		)
- 	}
-});
-
-var Main = React.createClass({
-	render: function(){
- 		return React.createElement('div', {style: {border: "1px solid black", width:"79%", height:"100%", display:"inline-block"}}, 
- 			React.createElement('ul',null,
- 				React.createElement(ListItemWithLink,{name: "Link!"})
- 			)
- 		)
- 	}
+  render: function(){
+            return React.createElement('nav', {style: {border: "1px solid pink"}},
+              React.createElement('ul', null,
+                React.createElement(ListItemWithLink,{name:"home"}),
+                React.createElement(ListItemWithLink,{name:"logout"}),
+                React.createElement(ListItemWithLink,{name:"about me"})
+              )
+            )
+          }
 });
 
 var ListItemWithLink = React.createClass({
-	render: function(){
-		return React.createElement('li', null,
-			React.createElement("a", {href: ""}, this.props.name)
-		)
-	}
+  render: function(){
+            return React.createElement('li', null,
+              React.createElement('a', {href:''}, this.props.name)
+            )
+          }
 });
 
-var NinjaComponent = React.createElement('div',null,
-	React.createElement(Header),
-	React.createElement(Sidebar),
-	React.createElement(Main)
-)
-ReactDOM.render(NinjaComponent, document.getElementById('react-container'));
+var Contact = React.createClass({
+  render: function(){
+            return React.createElement('div', null,
+              React.createElement('p', null, 'cmead@codingdojo.com')
+            )
+          }
+});
+
+var FakeBlog = React.createClass({
+  render: function(){
+            return React.createElement('div', null,
+              React.createElement(Header),
+              React.createElement(Sidebar),
+              React.createElement(MainContent),
+              React.createElement('footer', {style :{border: "1px solid black", textAlign: "center", padding: "15px"}},
+                "©2016",
+                React.createElement(Contact)
+              )
+            )
+          }
+});
+
+var MainContent = React.createClass({
+  render: function(){
+            return React.createElement('div', {style: {display: 'inline-block', verticalAlign: 'top', margin: "10px", padding:'15px', border: "1px solid green", width: '700px'}},
+              React.createElement('h1', null, "Main Content Headline"),
+              React.createElement('p', null, "Main content paragraph.")
+            )
+          }
+});
+
+var Sidebar = React.createClass({
+  render: function(){
+            return React.createElement('div', {style: {display: 'inline-block', width: "300px", margin: "10px", padding:'15px', border: "1px solid purple"}},
+              React.createElement('h1', null, "Sidebar Headline"),
+              React.createElement(ListItemWithLink, {name: "Sidebar link"}),
+              React.createElement(ListItemWithLink, {name: "Another sidebar link"}),
+              React.createElement(ListItemWithLink, {name: "Yet another sidebar link"})
+            )
+          }
+});
+
+ReactDOM.render(React.createElement(FakeBlog), document.getElementById('react-container'));
