b57bb2f1c4b3e120abd5b8d17790454284d93724
benmier <bencmier@gmail.com>
2016-04-20 16:24:22 -0700

    Updated react demo project

--- React Demo/main.js
+++ React Demo/main.js
@@ -1,5 +1,17 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+
 var NinjaTitle = React.createClass({
  render: function(){
-  return React.createElement('h1', null, this.props.title);
+  return <h1>{this.props.title}</h1>;
  }
 });
+
+var NinjaComponent = React.createElement('div',
+    null,
+    React.createElement(NinjaTitle, { title: 'React Ninja'}),
+    React.createElement(NinjaTitle, { title: 'Angular Ninja'}),
+    React.createElement(NinjaTitle, { title: 'Backbone Ninja'}),
+    React.createElement(NinjaTitle, { title: 'jQuery Ninja'})
+    )
+ReactDOM.render(NinjaComponent, document.getElementById('react-container'));
