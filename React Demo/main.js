8a377a8c44f4f49edfc3bed76a7ec84e597c8f0b
benmier <bencmier@gmail.com>
2016-04-20 17:34:35 -0700

    Updated react plotting blocks project

--- React Demo/main.js
+++ React Demo/main.js
@@ -12,11 +12,12 @@ var NinjaComponent = React.createClass({
   render: function(){
 	return (
 	  <div>
-		<div style={{backgroundColor: "green", height:"100px", width:"900px"}}></div>
-		<div style={{backgroundColor: "blue", height:"500px", width:"100px", margin:"20px 20px 0px 0px", display:"inline-block"}}></div>
-		<div style={{backgroundColor: "red", height:"500px", width:"780px", display:"inline-block"}}>
-			<div style={{backgroundColor: "yellow", height:"150px", width:"150px", margin:"20px", display:"inline"}}></div>
-			<div style={{backgroundColor: "yellow", height:"150px", width:"150px", margin:"20px", display:"block"}}></div>
+		<div style={{backgroundColor: "green", height:"100px", width:"900px", marginBottom:"20px"}}></div>
+		<div style={{backgroundColor: "blue", height:"500px", width:"100px", marginRight:"20px", display:"inline-block"}}></div>
+		<div style={{backgroundColor: "red", height:"500px", width:"780px", display:"inline-block", verticalAlign:"top"}}>
+			<div style={{backgroundColor: "yellow", height:"225px", width:"225px", margin:"20px 0px 20px 20px", display:"inline-block"}}></div>
+			<div style={{backgroundColor: "yellow", height:"225px", width:"225px", margin:"20px 0px 20px 20px", display:"inline-block"}}></div>
+			<div style={{backgroundColor: "yellow", height:"225px", width:"225px", margin:"20px 0px 20px 20px", display:"inline-block"}}></div>
 		</div>
 	  </div>
 	)
