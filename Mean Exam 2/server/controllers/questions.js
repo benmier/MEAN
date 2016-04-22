417ce1f0e19d6e50e02781dd21b151ca3491fccb
benmier <bencmier@gmail.com>
2016-04-22 11:44:38 -0700

    Updated mean black belt exam 2

--- Mean Exam 2/server/controllers/questions.js
+++ Mean Exam 2/server/controllers/questions.js
@@ -5,7 +5,7 @@ var Questions = mongoose.model('Questions');
 module.exports = {
 
 	show3: function(req,res){
-        Users.findRandom({},{},{limit: 3}, function(err,data){
+        Questions.findRandom({},{},{limit: 3}, function(err,data){
             if(err)
                 console.log(err);
             else
@@ -13,4 +13,31 @@ module.exports = {
         });
     },
 
+    create: function(req,res){
+        Questions.findOne({question:req.body.question},function(err,data){
+            if(err)
+                console.log(err);
+            else{
+                if(data){
+                    res.json(data);
+                }
+                else{
+                    var question = new Questions(req.body) 
+                    question.save(function(err){
+                        if(err)
+                            console.log(err);
+                        else{
+                            Questions.findOne({},function(err,data){
+                                if(err)
+                                    console.log(err);
+                                else{
+                                    res.json(data);
+                                }
+                            }).sort({_id:-1});
+                        };
+                    });
+                }
+            }
+        });
+    }
 }
