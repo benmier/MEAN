1fcbc6bed71db4ed1168cfb5fd9de895160977c9
benmier <bencmier@gmail.com>
2016-04-22 10:17:34 -0700

    Updated mean black belt exam 2

--- Mean Exam 2/server/models/question.js
+++ Mean Exam 2/server/models/question.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 var QuestionSchema = new mongoose.Schema({
-	
+	question: String,
+	correct: String,
+	fake1: String,
+	fake2: String
 },{timestamps:true});
 
 var Questions = mongoose.model('Questions', QuestionSchema);
--- Mean Exam 2/server/models/quiz.js
+++ Mean Exam 2/server/models/quiz.js
@@ -1,7 +1,24 @@
 var mongoose = require('mongoose');
 
 var QuizSchema = new mongoose.Schema({
-	
+	question1: [
+		{question: String},
+		{correct: String},
+		{fake1: String},
+		{fake2: String}
+	],
+	question2: [
+		{question: String},
+		{correct: String},
+		{fake1: String},
+		{fake2: String}
+	],
+	question3: [
+		{question: String},
+		{correct: String},
+		{fake1: String},
+		{fake2: String}
+	]
 },{timestamps:true});
 
 var Quizzes = mongoose.model('Quizzes', QuizSchema);
