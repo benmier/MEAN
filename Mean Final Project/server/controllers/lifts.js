227f9169bb50102250013dd3c18434ef0f4610ec
benmier <bencmier@gmail.com>
2016-04-25 11:16:14 -0700

    Updated mean final project

---
+++ Mean Final Project/server/controllers/lifts.js
@@ -0,0 +1,54 @@
+var mongoose = require('mongoose');
+var Lifts = mongoose.model('Lifts');
+
+module.exports = {
+
+	show: function(req,res){
+        Lifts.find({},function(err,data){
+            if(err)
+                console.log(err);
+            else
+                res.json(data);
+        });
+    },
+
+    showOne: function(req,res){
+        Lifts.findOne({_id:req.params.id},function(err,data){
+            if(err)
+                console.log(err);
+            else
+                res.json(data);
+        });
+    },
+
+    create: function(req,res){
+        Lifts.findOne({name:req.body.name},function(err,data){
+            if(err)
+                console.log(err);
+            else{
+                if(data){
+                    res.json(data);
+                }
+                else{
+                    var lift = new Lifts(req.body) 
+                    lift.save(function(err){
+                        if(err)
+                            console.log(err);
+                        else{
+                            Lifts.findOne({},function(err,data){
+                                if(err)
+                                    console.log(err);
+                                else{
+                                    res.json(data);
+                                }
+                            }).sort({_id:-1});
+                        };
+                    });
+                }
+            }
+        });
+    },
+
+
+
+}
