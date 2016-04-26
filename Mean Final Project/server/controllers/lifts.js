bde035c41c73354d04501a63521543ecfd8d4226
benmier <bencmier@gmail.com>
2016-04-25 17:51:28 -0700

    Updated mean final project

--- Mean Final Project/server/controllers/lifts.js
+++ Mean Final Project/server/controllers/lifts.js
@@ -16,8 +16,43 @@ module.exports = {
         Lifts.findOne({name:req.params.name},function(err,data){
             if(err)
                 console.log(err);
-            else
+            else{
+                if(data.main_muscle=="Abdominals")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/13.gif"
+                if(data.main_muscle=="Bicepts")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/15.gif"
+                if(data.main_muscle=="Triceps")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/10.gif"
+                if(data.main_muscle=="Chest")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/1.gif"
+                if(data.main_muscle=="Quadriceps")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/7.gif"
+                if(data.main_muscle=="Hamstrings")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/8.gif"
+                if(data.main_muscle=="Calves")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/9.gif"
+                if(data.main_muscle=="Middle Back")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/4.gif"
+                if(data.main_muscle=="Shoulders")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/12.gif"
+                if(data.main_muscle=="Traps")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/11.gif"
+                if(data.main_muscle=="Adductors")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/17.gif"
+                if(data.main_muscle=="Lower Back")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/5.gif"
+                if(data.main_muscle=="Glutes")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/14.gif"
+                if(data.main_muscle=="Lats")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/3.gif"
+                if(data.main_muscle=="Forearms")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/2.gif"
+                if(data.main_muscle=="Neck")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/6.gif"
+                if(data.main_muscle=="Abductors")
+                    data.muscle_img = "http://assets.bodybuilding.com/images/trackers/exercise/heatmap/18.gif"
                 res.json(data);
+            }
         });
     },
 
