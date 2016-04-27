b0a0b1a2fc1385eaf82e841d34ac51902498addb
benmier <bencmier@gmail.com>
2016-04-27 15:24:57 -0700

    Updated mean final project

--- Mean Final Project/client/script.js
+++ Mean Final Project/client/script.js
@@ -165,10 +165,11 @@ myApp.factory('liftFactory', function($http){
                     if(lift.type=="Cardio")
                         factory.exercises[i].sets.push({duration:null,hour:0,min:0,sec:0,pace:null});
                     else{
-                        factory.exercises[i].sets.push({
-                            reps:"$scope.newWorkout.exercise[lift.name].set[Object.keys($scope.newWorkout.exercise[lift.name].set)[Object.keys($scope.newWorkout.exercise[lift.name].set).length-1]].reps",
-                            lbs:"$scope.newWorkout.exercise[lift.name].set[Object.keys($scope.newWorkout.exercise[lift.name].set)[Object.keys($scope.newWorkout.exercise[lift.name].set).length-1]].lbs"
-                        });
+                        factory.exercises[i].sets.push({reps:null,lbs:null});
+                        // factory.exercises[i].sets.push({
+                            // reps:$scope.newWorkout.exercise[lift.name].set[Object.keys($scope.newWorkout.exercise[lift.name].set)[Object.keys($scope.newWorkout.exercise[lift.name].set).length-1]].reps,
+                            // lbs:$scope.newWorkout.exercise[lift.name].set[Object.keys($scope.newWorkout.exercise[lift.name].set)[Object.keys($scope.newWorkout.exercise[lift.name].set).length-1]].lbs
+                        // });
                     }
                 }
             }
